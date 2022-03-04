932e4366efe23aef4218d261b902d54702dc8b01
momoterry <43855737+momoterry@users.noreply.github.com>
2022-03-04 13:52:54 +0800

    FlashFX 加入可隨機上下或左右翻轉的功能

--- Assets/Code/FlashFX.cs
+++ Assets/Code/FlashFX.cs
@@ -6,6 +6,8 @@ public class FlashFX : MonoBehaviour
 {
     // Start is called before the first frame update
     public float LifeTime = 0.2f;
+    public bool randomHoriFlip = false;
+    public bool randomVertiFlip = false;
 
 
     private float myTime = 0.0f;
@@ -13,6 +15,14 @@ public class FlashFX : MonoBehaviour
     void Start()
     {
         myTime = LifeTime;
+        if (randomHoriFlip && Random.Range(0.0f, 1.0f) > 0.5f)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+        if (randomVertiFlip && Random.Range(0.0f, 1.0f) > 0.5f)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+        }
     }
 
     // Update is called once per frame
