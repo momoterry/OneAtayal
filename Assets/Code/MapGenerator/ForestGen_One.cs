50caad9eed5b1e2d33386ab5222e411f41c2b486
momoterry <tyeh777@gmail.com>
2022-03-17 22:55:41 +0800

    森林 Scene 能動態生成 room 並南北相接
    加入 RoomController 來決定南、北門
    加入新的 ForestGen 來產生森林地圖

---
+++ Assets/Code/MapGenerator/ForestGen_One.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ForestGen_One : MapGeneratorBase
+{
+    public NavMeshSurface2d theSurface2D;
+    public GameObject[] roomRefs;
+
+    public int roomToGen = 2;
+
+    public RoomController startRC;
+
+    int toBuild = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: 還不知道為什麼，但等多個 Frame 之後才產生 NavMesh 不然會出錯
+        if (toBuild > 0)
+        {
+            toBuild--;
+            if (toBuild == 0)
+                theSurface2D.BuildNavMeshAsync();
+        }
+    }
+
+
+    public override void BuildAll(int buildLevel = 1)
+    {
+        base.BuildAll(buildLevel);
+
+        Vector3 pos = transform.position;
+        if (startRC && startRC.northDoor)
+        {
+            pos = startRC.northDoor.position;
+        }
+
+        for (int i=0; i<roomToGen; i++)
+        {
+            //pos = pos + new Vector3(0, 20.0f, 0);
+            if (roomRefs[0])
+            {
+                GameObject ro = Instantiate(roomRefs[0], pos, Quaternion.identity, null);
+                if (ro)
+                {
+                    RoomController rc = ro.GetComponent<RoomController>();
+                    if (rc && rc.southDoor)
+                    {
+                        pos += rc.transform.position - rc.southDoor.position;
+                        ro.transform.position = pos;
+                    }
+                    else
+                    {
+                        print("Room Error !! No RoomController or SouthDoor !!");
+                    }
+                    ro.transform.SetParent(theSurface2D.gameObject.transform);
+                    if (rc && rc.northDoor)
+                    {
+                        pos += rc.northDoor.position - rc.transform.position;
+                    }
+                    else
+                    {
+                        print("Room Error !! No RoomController or NorthDoor !!");
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Code/MapGenerator/RoomController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform southDoor;
+    public Transform northDoor;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
