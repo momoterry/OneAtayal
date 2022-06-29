289de7550eaf7fe6a41de4c7c37807d7e04e9f36
momoterry <43855737+momoterry@users.noreply.github.com>
2022-06-29 11:45:04 +0800

    消除 Doll 的功能 (點擊刪除)

---
+++ Assets/Code/Doll/DollCanceller.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollCanceller : MonoBehaviour
+{
+    protected float clickRange = 1.0f;
+    protected DollSelector mySelector;
+
+    public void InitSelector(DollSelector selector)
+    {
+        mySelector = selector;
+    }
+
+    private void OnMouseDown()
+    {
+        //print("I got Clicked !!!!!!!!");
+        Vector3 mPos = Input.mousePosition;
+        Vector3 wPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        wPos.y = 0;
+
+        Vector3 myPos = transform.position;
+        wPos.y = 0;
+        myPos.y = 0;
+
+        float dis2 = (wPos - myPos).sqrMagnitude;
+        if (dis2 < clickRange * clickRange)
+        {
+            OnOK();
+        }
+        else
+        {
+            OnCancel();
+        }
+
+    }
+
+    public void OnCancel()
+    {
+        if (mySelector)
+        {
+            mySelector.OnCancellerCancel();
+        }
+        Destroy(gameObject);
+    }
+
+    public void OnOK()
+    {
+        if (mySelector)
+        {
+            mySelector.OnCancellerOK();
+        }
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if (mySelector)
+            mySelector.OnCancellerCancel();
+    }
+
+}
--- Assets/Code/Doll/DollSelector.cs
+++ Assets/Code/Doll/DollSelector.cs
@@ -4,20 +4,75 @@ using UnityEngine;
 
 public class DollSelector : MonoBehaviour
 {
+    public GameObject cancelObjRef;
+    protected float cancelButtonHeight = 1.5f;
+
+    protected DollCanceller myCanceller;
+
+    static DollCanceller theCanceller = null;        //只允許一份存在
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //shilftPos = Vector3.forward * cacelButtonHeight;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //TODO: 設定 Canceller 的位置
+        if (myCanceller)
+        {
+            myCanceller.gameObject.transform.position = transform.position + Vector3.forward * cancelButtonHeight;
+        }
     }
 
     private void OnMouseDown()
     {
-        print("我被選中了!!");
+        if (theCanceller)
+        {
+            //有存在的 Canceller, 先移除
+            theCanceller.OnCancel();
+            theCanceller = null;
+        }
+
+        if (cancelObjRef && !myCanceller)
+        {
+            GameObject myCancelObj = BattleSystem.GetInstance().SpawnGameplayObject(cancelObjRef, transform.position + Vector3.forward * cancelButtonHeight);
+            myCanceller = myCancelObj.GetComponent<DollCanceller>();
+            if (myCanceller)
+            {
+                myCanceller.InitSelector(this);                
+                theCanceller = myCanceller;
+
+            }
+            else
+            {
+                print("ERROR !! cancelObjRef 沒有 DollCanceller!! ");
+                Destroy(myCancelObj);
+            }
+        }
+    }
+
+    //====
+    public void OnCancellerCancel()
+    {
+        if (myCanceller == theCanceller)
+        {
+            theCanceller = null;
+        }
+        myCanceller = null;
+    }
+
+    public void OnCancellerOK()
+    {
+        if (myCanceller == theCanceller)
+        {
+            theCanceller = null;
+        }
+
+        myCanceller = null;
+        //TODO: 叫 Doll 來執行
+        Destroy(gameObject);
     }
 }
