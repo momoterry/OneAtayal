7346b9e71f0b94ba1b8f77c123c372d6963c9c76
momoterry_LilithNB <43855737+momoterry@users.noreply.github.com>
2022-07-15 10:33:58 +0800

    DamageArea 初稿

---
+++ Assets/Code/bullet/DamageArea.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageArea : AreaEffectBase
+{
+    //public float damageRatio = 0.02f;
+    public GameObject damageFX;
+
+    protected override void ApplyEffect(GameObject obj)
+    {
+        //float healAbsoluteValue = baseDamage;
+
+        Damage myDamage;
+        myDamage.damage = baseDamage;
+
+        //PlayerControllerBase pc = obj.GetComponent<PlayerControllerBase>();
+        //if (pc)
+        //{
+        //    //pc.DoHeal(pc.GetHPMax() * healRatio + healAbsoluteValue);
+        //}
+
+        //HitBody body = obj.GetComponent<HitBody>();
+        //if (body)
+        //{
+        //    //body.DoHeal(body.GetHPMax() * healRatio + healAbsoluteValue);
+        //    obj.SendMessage("OnDamage", myDamage)
+        //}
+
+        obj.SendMessage("OnDamage", myDamage);
+
+        if (damageFX)
+            BattleSystem.GetInstance().SpawnGameplayObject(damageFX, obj.transform.position, obj.transform);
+    }
+}
