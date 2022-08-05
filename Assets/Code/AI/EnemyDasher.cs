3bc113c9e2ed5675508a2b8934999556e061d986
momoterry <tyeh777@gmail.com>
2022-08-05 17:43:37 +0800

    EnemyDasher 程式架構

---
+++ Assets/Code/AI/EnemyDasher.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDasher : Enemy
+{
+    protected override void DoOneAttack()
+    {
+
+        print("EnemyDasher Attack !!!!");
+    }
+}
