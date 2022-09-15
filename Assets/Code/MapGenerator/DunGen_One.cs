8ff053c3d2bda13e135cbb762ba1c848048f67d1
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2022-09-15 12:31:32 +0800

    初步的 Random 結果

--- Assets/Code/MapGenerator/DunGen_One.cs
+++ Assets/Code/MapGenerator/DunGen_One.cs
@@ -9,6 +9,12 @@ public class DunGen_One : MapGeneratorBase
 
     protected int RoomNum = 3;
 
+    protected enum ExtendType
+    {
+        N,W,E,
+        NB, WB, EB
+    }
+
     protected List<GameObject> roomList = new List<GameObject>();
 
     int toBuild = 5;
@@ -39,102 +45,126 @@ public class DunGen_One : MapGeneratorBase
         if (!initRD)
             return;
 
-        GameObject currRoom = initRD.gameObject;
-        GameObject newRoom = null;
+        ExtendType[,] extendData = new ExtendType[,] {
+            { ExtendType.N, ExtendType.W, ExtendType.WB, ExtendType.N },
+            { ExtendType.N, ExtendType.W, ExtendType.NB, ExtendType.W },
+            { ExtendType.N, ExtendType.E, ExtendType.EB, ExtendType.N },
+            { ExtendType.N, ExtendType.E, ExtendType.NB, ExtendType.E },
+            { ExtendType.N, ExtendType.N, ExtendType.WB, ExtendType.N },
+            { ExtendType.N, ExtendType.N, ExtendType.EB, ExtendType.N },
+        };
 
-        for (int i= 0; i< RoomNum; i++)
+        int rdPath = Random.Range(0, extendData.GetLength(0)-1);
+
+        print("DATA " + extendData.GetLength(0) + "  " + extendData.GetLength(1));
+
+        //ExtendType[] extendList = { ExtendType.N, ExtendType.W, ExtendType.WB, ExtendType.N };
+
+
+        GameObject currRoom = initRD.gameObject;
+        GameObject newRoom;
+
+        for (int i = 0; i < extendData.GetLength(1); i++)
         {
-            newRoom = CreateRoom(roomRef, currRoom, DoorDir.N, DoorDir.S);
+            DoorDir dFron = DoorDir.N;
+            DoorDir dTo = DoorDir.S;
+            bool isMain = true;
+            switch (extendData[rdPath, i])
+            {
+                case ExtendType.N:
+                    dFron = DoorDir.N;
+                    dTo = DoorDir.S;
+                    break;
+                case ExtendType.W:
+                    dFron = DoorDir.W1;
+                    dTo = DoorDir.E2;
+                    break;
+                case ExtendType.E:
+                    dFron = DoorDir.E1;
+                    dTo = DoorDir.W2;
+                    break;
+                case ExtendType.NB:
+                    dFron = DoorDir.N;
+                    dTo = DoorDir.S;
+                    isMain = false;
+                    break;
+                case ExtendType.WB:
+                    dFron = DoorDir.W1;
+                    dTo = DoorDir.E2;
+                    isMain = false;
+                    break;
+                case ExtendType.EB:
+                    dFron = DoorDir.E1;
+                    dTo = DoorDir.W2;
+                    isMain = false;
+                    break;
+            }
+
+            newRoom = CreateRoom(roomRef, currRoom, dFron, dTo);
             if (newRoom)
             {
                 print("NewRoom: " + newRoom.transform.position);
                 roomList.Add(newRoom);
-                //newRoom.transform.SetParent(theSurface2D.gameObject.transform);
             }
             else
             {
                 print("ERROR !!!! CreatRoom Fail....");
                 return;
             }
-            currRoom = newRoom;
-
-            if (i == 0)
+            if (isMain)
             {
-                //Try Branch
-                roomList.Add(CreateRoom(roomRef, newRoom, DoorDir.W1, DoorDir.E2));
-                roomList.Add(CreateRoom(roomRef, newRoom, DoorDir.E2, DoorDir.W1));
+                currRoom = newRoom;
             }
+
         }
 
-        foreach¡@(GameObject ro in roomList)
+        foreach (GameObject ro in roomList)
         {
             ro.transform.SetParent(theSurface2D.transform);
         }
 
-        //Vector3 pos = transform.position;
-
-        //if (initRD )
-        //{
-        //    pos = initRD.GetDoorPos(DoorDir.N);
-        //}
-
-        //if (roomRef == null)
-        //    return;
-
-        //for (int i = 0; i < RoomNum; i++)
-        //{
-        //    //GameObject ro = Instantiate(roomRef, pos, rm, null);
-        //    GameObject ro = BattleSystem.GetInstance().SpawnGameplayObject(roomRef, pos);
-        //    if (ro)
-        //    {
-        //        roomList.Add(ro);
-
-        //        //RoomController rc = ro.GetComponent<RoomController>();
-        //        RoomDungeon rd = ro.GetComponent<RoomDungeon>();
-        //        if (rd)
-        //        {
-        //            pos += rd.transform.position - rd.GetDoorPos(DoorDir.S);
-        //            ro.transform.position = pos;
-
-        //            rd.SetDoorStatus(DoorDir.S, true);
-        //        }
-        //        else
-        //        {
-        //            print("Room Error !! No RoomDungeon !!");
-        //        }
-        //        ro.transform.SetParent(theSurface2D.gameObject.transform);
-
-        //        //Gameplay
-        //        //GameObject go = Instantiate(gameplayRefs[i], pos, rm, null);
-        //        //if (go)
-        //        //    go.transform.SetParent(ro.transform);
-
-        //        if (i == 0)
-        //        {
-        //            //Try Branch
-        //            CreateRoom(roomRef, ro, DoorDir.W1, DoorDir.E2);
-        //            CreateRoom(roomRef, ro, DoorDir.E2, DoorDir.W1);
-        //        }
-
-        //        if (rd)
-        //        {
-        //            pos += rd.GetDoorPos(DoorDir.N) - rd.transform.position;
-        //            if (i != RoomNum - 1)
-        //            {
-        //                rd.SetDoorStatus(DoorDir.N, true);
-        //            }
-        //        }
-        //        else
-        //        {
-        //            print("Room Error !! No RoomDungeon !!");
-        //        }
-
-        //    }
-
-
-        //}
-
     }
+
+    //public override void BuildAll(int buildLevel = 1)
+    //{
+    //    base.BuildAll(buildLevel);
+
+    //    if (!initRD)
+    //        return;
+
+    //    GameObject currRoom = initRD.gameObject;
+    //    GameObject newRoom = null;
+
+    //    for (int i= 0; i< RoomNum; i++)
+    //    {
+    //        newRoom = CreateRoom(roomRef, currRoom, DoorDir.N, DoorDir.S);
+    //        if (newRoom)
+    //        {
+    //            print("NewRoom: " + newRoom.transform.position);
+    //            roomList.Add(newRoom);
+    //            //newRoom.transform.SetParent(theSurface2D.gameObject.transform);
+    //        }
+    //        else
+    //        {
+    //            print("ERROR !!!! CreatRoom Fail....");
+    //            return;
+    //        }
+    //        currRoom = newRoom;
+
+    //        if (i == 0)
+    //        {
+    //            //Try Branch
+    //            roomList.Add(CreateRoom(roomRef, newRoom, DoorDir.W1, DoorDir.E2));
+    //            roomList.Add(CreateRoom(roomRef, newRoom, DoorDir.E2, DoorDir.W1));
+    //        }
+    //    }
+
+    //    foreach¡@(GameObject ro in roomList)
+    //    {
+    //        ro.transform.SetParent(theSurface2D.transform);
+    //    }
+
+    //}
 
     protected GameObject CreateRoom( GameObject roRef, GameObject fromRoom, DoorDir fromDoor, DoorDir toDoor)
     {
