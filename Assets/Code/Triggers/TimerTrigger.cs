ef3b280bb2cdd1cd6debf66afbfbe90abf27792d
momoterry_Home <43855737+momoterry@users.noreply.github.com>
2022-10-05 11:54:40 +0800

    timerTrigger 初步架構

--- Assets/Code/Triggers/TimerTrigger.cs
+++ Assets/Code/Triggers/TimerTrigger.cs
@@ -7,13 +7,14 @@ public class TimerTrigger : MonoBehaviour
     [System.Serializable]
     public class StepInfo
     {
-        float timeToWait = 0;
-        public GameObject[] TriggerTargets;
+        public float timeToWait = 0;
+        public GameObject TriggerTarget;
     }
 
     public StepInfo[] allSteps;
 
     public bool isLoop = false;
+    public float timeAfterLoop = 0;
 
     protected enum Phase
     {
@@ -25,13 +26,13 @@ public class TimerTrigger : MonoBehaviour
     protected Phase currPhase = Phase.NONE;
     protected Phase nextPhase = Phase.WAIT;
 
-    protected int currTrigger = 0;
+    protected int currStep = 0;
     protected float stepTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -48,7 +49,34 @@ public class TimerTrigger : MonoBehaviour
 
     void UpdateActive()
     {
-        
+        stepTime += Time.deltaTime;
+        if (stepTime >= allSteps[currStep].timeToWait)
+        {
+            if (allSteps[currStep].TriggerTarget)
+            {
+                allSteps[currStep].TriggerTarget.SendMessage("OnTG", gameObject, SendMessageOptions.DontRequireReceiver);
+                currStep++;
+                stepTime = 0;
+                if (currStep >= allSteps.Length)
+                {
+                    if (isLoop)
+                    {
+                        currStep = 0;
+                    }
+                    else
+                    {
+                        nextPhase = Phase.FINISH;
+                    }
+                }
+            }
+        }
     }
 
+    void OnTG(GameObject whoTG)
+    {
+        if (currPhase == Phase.WAIT)
+        {
+            nextPhase = Phase.ACTIVE;
+        }
+    }
 }
