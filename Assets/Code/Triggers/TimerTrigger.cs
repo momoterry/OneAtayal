25a66a25ae0fef38f20ee08318aac6f13e6f548c
momoterry_Home <43855737+momoterry@users.noreply.github.com>
2022-10-05 10:34:33 +0800

    TimerTrigger 初步架構

--- Assets/Code/Triggers/TimerTrigger.cs
+++ Assets/Code/Triggers/TimerTrigger.cs
@@ -11,9 +11,22 @@ public class TimerTrigger : MonoBehaviour
         public GameObject[] TriggerTargets;
     }
 
-    public StepInfo allSteps;
+    public StepInfo[] allSteps;
 
     public bool isLoop = false;
+
+    protected enum Phase
+    {
+        NONE,
+        WAIT,
+        ACTIVE,
+        FINISH,
+    }
+    protected Phase currPhase = Phase.NONE;
+    protected Phase nextPhase = Phase.WAIT;
+
+    protected int currTrigger = 0;
+    protected float stepTime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +37,18 @@ public class TimerTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (currPhase)
+        {
+            case Phase.ACTIVE:
+                UpdateActive();
+                break;
+        }
+        currPhase = nextPhase;
+    }
+
+    void UpdateActive()
+    {
         
     }
+
 }
