7ba810dababc6597143e1fdb8f2b78a47f49adc8
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2022-10-24 15:44:14 +0800

    串接白火巫靈

--- Assets/Code/Doll/DollRecovery.cs
+++ Assets/Code/Doll/DollRecovery.cs
@@ -106,17 +106,17 @@ public class DollRecovery : MonoBehaviour
 
     void StartSpawn()
     {
-        //*Debug
+        /*Debug
         if (GameSystem.GetPlayerData().GetCurrDollNum() == 0)
         {
             GameSystem.GetPlayerData().AddUsingDoll("DollBlackStone");
             GameSystem.GetPlayerData().AddUsingDoll("DollBlackStone");
-            //GameSystem.GetPlayerData().AddUsingDoll("DollBlackStone");
-            //GameSystem.GetPlayerData().AddUsingDoll("DollBlackStone");
-            GameSystem.GetPlayerData().AddUsingDoll("DollOne");
-            GameSystem.GetPlayerData().AddUsingDoll("DollOne");
-            GameSystem.GetPlayerData().AddUsingDoll("DollOne");
-            GameSystem.GetPlayerData().AddUsingDoll("DollOne");
+            GameSystem.GetPlayerData().AddUsingDoll("DollBlackStone");
+            GameSystem.GetPlayerData().AddUsingDoll("DollBlackStone");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollOne");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollOne");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollOne");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollOne");
 
             //GameSystem.GetPlayerData().AddUsingDoll("DollAce");
             //GameSystem.GetPlayerData().AddUsingDoll("DollAce");
@@ -125,16 +125,18 @@ public class DollRecovery : MonoBehaviour
 
 
 
+            //GameSystem.GetPlayerData().AddUsingDoll("DollWhiteFire");
             GameSystem.GetPlayerData().AddUsingDoll("DollWhiteFire");
-            GameSystem.GetPlayerData().AddUsingDoll("DollWhiteFire");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollWhiteFire");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollWhiteFire");
 
 
-            GameSystem.GetPlayerData().AddUsingDoll("DollFire");
-            GameSystem.GetPlayerData().AddUsingDoll("DollFire");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollFire");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollFire");
             GameSystem.GetPlayerData().AddUsingDoll("DollLeaf");
             GameSystem.GetPlayerData().AddUsingDoll("DollLeaf");
-            GameSystem.GetPlayerData().AddUsingDoll("DollLeaf");
-            GameSystem.GetPlayerData().AddUsingDoll("DollLeaf");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollLeaf");
+            //GameSystem.GetPlayerData().AddUsingDoll("DollLeaf");
 
         }
         //*/
---
+++ _Data/FX/FirePillar/batchWhite.bat
@@ -0,0 +1,4 @@
+@set ASEPRITE="C:\Program Files (x86)\Steam\steamapps\common\Aseprite\aseprite.exe"
+%ASEPRITE% -b --tag "FireOn" FirePillarWhite.aseprite --sheet  firePillarWhite_FireOn.png
+%ASEPRITE% -b --tag "Loop" FirePillarWhite.aseprite --sheet  firePillarWhite_Loop.png
+%ASEPRITE% -b --tag "FireOff" FirePillarWhite.aseprite --sheet  firePillarWhite_FireOff.png
