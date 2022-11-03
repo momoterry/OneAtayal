fc86eaa1283469a6897496c9a8c9c10a50b0c748
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2022-11-03 11:39:15 +0800

    初步的 CameraBlend 功能

---
+++ Assets/Code/Triggers/BoxBlender.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBlender : MonoBehaviour
+{
+    public bool blendHori = true;   //垂直改變為 false
+
+    protected BoxCollider areaBox;
+    protected PlayerControllerBase thePlayerIn = null;
+
+    protected float v1;
+    protected float v2;
+
+    protected float ratio;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        areaBox = GetComponent<BoxCollider>();
+        if (!areaBox)
+        {
+            print("ERROR!! BoxBlender need a BoxCollider !!!!!");
+            return;
+        }
+
+        if (blendHori)
+        {
+            v1 = areaBox.transform.position.x + areaBox.center.x - (areaBox.size.x * 0.5f);
+            v2 = areaBox.transform.position.x + areaBox.center.x + (areaBox.size.x * 0.5f);
+        }
+        else
+        {
+            v1 = areaBox.transform.position.z + areaBox.center.z - (areaBox.size.z * 0.5f);
+            v2 = areaBox.transform.position.z + areaBox.center.z + (areaBox.size.z * 0.5f);
+        }
+
+    }
+
+    protected virtual void CalcBlendRatio()
+    {
+        if (thePlayerIn && areaBox)
+        {
+            float vo;
+            if (blendHori)
+            {
+                vo = thePlayerIn.transform.position.x;
+            }
+            else
+            {
+                vo = thePlayerIn.transform.position.z;
+            }
+
+            ratio = (vo - v1) / (v2 - v1);
+
+            ratio = Mathf.Clamp(ratio, 0, 1.0f);
+        }
+    }
+
+    protected virtual void ApplyBlendResult() { }
+    //protected virtual void OnStartBlend() { }
+    //protected virtual void OnEndBlend() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (thePlayerIn)
+        {
+            CalcBlendRatio();
+            ApplyBlendResult();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        thePlayerIn = other.GetComponent<PlayerControllerBase>();
+
+        //OnStartBlend();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (thePlayerIn)
+        {
+            //離開時,確保一定是極端值
+            CalcBlendRatio();
+            ratio = ratio > 0.5f ? 1.0f : 0;
+            ApplyBlendResult();
+
+            //OnEndBlend();
+        }
+        thePlayerIn = null;
+    }
+}
---
+++ Assets/Code/Triggers/CameraBlender.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraBlender : BoxBlender
+{
+    protected float SizeAdd_1 = 0;
+    protected float SizeAdd_2 = 2.0f;
+
+    protected Vector3 OffsetAdd_1 = Vector3.zero;
+    protected Vector3 OffsetAdd_2 = new Vector3(2.0f, 0, -2.0f);
+
+    protected Camera theCamera;
+    protected BattleCamera theBattleCamera;
+
+    protected float originalSize;
+    protected Vector3 originalOffset;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        print("CameraBlender::Start");
+        theCamera = Camera.main;
+        originalSize = theCamera.orthographicSize;
+        theBattleCamera = theCamera.GetComponent<BattleCamera>();
+        if (theBattleCamera)
+        {
+            originalOffset = theBattleCamera.targetOffset;
+        }
+    }
+
+    //protected override void OnStartBlend()
+    //{
+    //    print("CameraBlender::OnStartBlend");
+    //    //theCamera = Camera.main;
+    //    //originalSize = theCamera.orthographicSize;
+    //    //theBattleCamera = theCamera.GetComponent<BattleCamera>();
+    //    //if (theBattleCamera)
+    //    //{
+    //    //    originalOffset = theBattleCamera.targetOffset;
+    //    //}
+    //}
+
+    //protected override void OnEndBlend()
+    //{
+    //    print("CameraBlender::OnEndBlend");
+    //}
+
+    protected override void ApplyBlendResult()
+    {
+        float sizeAdd = ratio * SizeAdd_2 + SizeAdd_1 * (1 - ratio);
+        Vector3 OffsetAdd = ratio * OffsetAdd_2 + OffsetAdd_1 * (1 - ratio);
+
+        theCamera.orthographicSize = originalSize + sizeAdd;
+        if (theBattleCamera)
+        {
+            theBattleCamera.targetOffset = originalOffset + OffsetAdd;
+        }
+
+    }
+}
