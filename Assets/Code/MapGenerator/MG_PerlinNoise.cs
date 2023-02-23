cf6ea73b02afc8e6341630e2c87f35b26ed60ac1
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2023-02-23 12:41:13 +0800

    完成基於 MG_TerrainBase 的 PerlinNoiseMap

--- Assets/Code/MapGenerator/MG_PerlinNoise.cs
+++ Assets/Code/MapGenerator/MG_PerlinNoise.cs
@@ -5,5 +5,31 @@ using UnityEngine.Tilemaps;
 
 public class MG_PerlinNoise : MG_TerrainBase
 {
-
+    public int NoiseScaleOn256 = 5;
+    public float highRatio = 0.35f;
+    public float lowRatio = 0.35f;
+    protected override void GenerateCellMap()
+    {
+        float noiseScale = (float)NoiseScaleOn256 * CellSize / 256.0f;
+        float randomSscale = 10.0f;
+        float xShift = Random.Range(0, NoiseScaleOn256 * randomSscale);
+        float yShift = Random.Range(0, NoiseScaleOn256 * randomSscale);
+        for (int x = theCellMap.GetXMin(); x <= theCellMap.GetXMax(); x++)
+        {
+            for (int y= theCellMap.GetYMin(); y <= theCellMap.GetYMax(); y++)
+            {
+                float rd = Mathf.PerlinNoise((float)x * noiseScale + xShift, (float)y * noiseScale + yShift);
+                if ( rd > 1.0f - highRatio)
+                {
+                    theCellMap.SetValue(x, y, 3);
+                }
+                else if (rd < lowRatio)
+                {
+                    theCellMap.SetValue(x, y, 2);
+                }
+                else
+                    theCellMap.SetValue(x, y, 1);
+            }
+        }
+    }
 }
