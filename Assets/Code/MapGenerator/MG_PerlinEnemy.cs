4597c4953fe7e1747cf1ff8ca051980546c3cde5
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2023-03-14 14:55:08 +0800

    用 Sprite 的方式解決 minimap 大小不為 2冪的問題

--- Assets/Code/MapGenerator/MG_PerlinEnemy.cs
+++ Assets/Code/MapGenerator/MG_PerlinEnemy.cs
@@ -30,14 +30,54 @@ public class MG_PerlinEnemy : MG_PerlinNoise
         if (MiniMapImageTest)
         {
             float tSep = Time.realtimeSinceStartup;
-            Texture2D newTexture = CreateTextures2();
-            MiniMapImageTest.sprite = Sprite.Create(newTexture, new Rect(0, 0, newTexture.width, newTexture.height), Vector2.zero);
+            //Texture2D newTexture = CreateTextures();
+            MiniMapImageTest.sprite = CreateMiniMapSprite();
             tSep = Time.realtimeSinceStartup - tSep;
             print("小地圖創建時間: " + tSep);
         }
     }
 
-    Texture2D CreateTextures2()
+    Sprite CreateMiniMapSprite()
+    {
+        OneMap theMap = theCellMap.GetOneMap();
+        int tWidth = Mathf.NextPowerOfTwo(theMap.mapWidth);
+        int tHeight = Mathf.NextPowerOfTwo(theMap.mapHeight);
+        Color[] colorMap = new Color[tWidth * tHeight];
+
+        for (int y = 0; y < theMap.mapHeight; y++)
+        {
+            for (int x = 0; x < theMap.mapWidth; x++)
+            {
+                int value = theMap.GetValue(x + theMap.xMin, y + theMap.yMin);
+                Color color = Color.black;
+                switch (value)
+                {
+                    case (int)MY_VALUE.NORMAL:
+                        color = Color.green;
+                        break;
+                    case (int)MY_VALUE.LOW:
+                        color = Color.yellow;
+                        break;
+                    case (int)MY_VALUE.HIGH:
+                    case (int)MY_VALUE.HIGH_2:
+                    case (int)MY_VALUE.HIGH_3:
+                        color = new Color(0, 0.5f, 0);
+                        break;
+
+                }
+                //color = new Color(0, 0.5f, 0);
+                colorMap[y * tWidth + x] = color;
+            }
+        }
+
+        Texture2D texture = new Texture2D(tWidth, tHeight);
+        texture.SetPixels(colorMap);
+        texture.Apply();
+        Sprite s = Sprite.Create(texture, new Rect(0, 0, theMap.mapWidth, theMap.mapHeight), Vector2.zero);
+        return s;
+    }
+
+    Texture2D CreateTextures()
     {
         OneMap theMap = theCellMap.GetOneMap();
         int tWidth = Mathf.NextPowerOfTwo(theMap.mapWidth);
@@ -77,40 +117,40 @@ public class MG_PerlinEnemy : MG_PerlinNoise
         return texture;
     }
 
-    Texture2D CreateTextures()
-    {
-        OneMap theMap = theCellMap.GetOneMap();
-        int tWidth = Mathf.NextPowerOfTwo(theMap.mapWidth);
-        int tHeight = Mathf.NextPowerOfTwo(theMap.mapHeight);
-        print("CreateTextures:" + theMap.mapWidth + ", " + theMap.mapHeight + " =>" + tWidth + ", " + tHeight);
-        Texture2D texture = new Texture2D(tWidth, tHeight, TextureFormat.ARGB32, false);
-        for (int x = 0; x < theMap.mapWidth; x++)
-        {
-            for (int y = 0; y < theMap.mapHeight; y++)
-            {
-                int value = theMap.GetValue(x+theMap.xMin, y+theMap.yMin);
-                Color color = Color.black;
-                switch (value)
-                {
-                    case (int)MY_VALUE.NORMAL:
-                        color = Color.green;
-                        break;
-                    case (int)MY_VALUE.LOW:
-                        color = Color.yellow;
-                        break;
-                    case (int)MY_VALUE.HIGH:
-                    case (int)MY_VALUE.HIGH_2:
-                    case (int)MY_VALUE.HIGH_3:
-                        color = new Color(0, 0.5f, 0);
-                        break;
+    //Texture2D CreateTextures_Old()
+    //{
+    //    OneMap theMap = theCellMap.GetOneMap();
+    //    int tWidth = Mathf.NextPowerOfTwo(theMap.mapWidth);
+    //    int tHeight = Mathf.NextPowerOfTwo(theMap.mapHeight);
+    //    print("CreateTextures:" + theMap.mapWidth + ", " + theMap.mapHeight + " =>" + tWidth + ", " + tHeight);
+    //    Texture2D texture = new Texture2D(tWidth, tHeight, TextureFormat.ARGB32, false);
+    //    for (int x = 0; x < theMap.mapWidth; x++)
+    //    {
+    //        for (int y = 0; y < theMap.mapHeight; y++)
+    //        {
+    //            int value = theMap.GetValue(x+theMap.xMin, y+theMap.yMin);
+    //            Color color = Color.black;
+    //            switch (value)
+    //            {
+    //                case (int)MY_VALUE.NORMAL:
+    //                    color = Color.green;
+    //                    break;
+    //                case (int)MY_VALUE.LOW:
+    //                    color = Color.yellow;
+    //                    break;
+    //                case (int)MY_VALUE.HIGH:
+    //                case (int)MY_VALUE.HIGH_2:
+    //                case (int)MY_VALUE.HIGH_3:
+    //                    color = new Color(0, 0.5f, 0);
+    //                    break;
 
-                }
-                //color = new Color(0, 0.5f, 0);
-                texture.SetPixel(x, y, color);
-            }
-        }
-        texture.Apply();
-        return texture;
-    }
+    //            }
+    //            //color = new Color(0, 0.5f, 0);
+    //            texture.SetPixel(x, y, color);
+    //        }
+    //    }
+    //    texture.Apply();
+    //    return texture;
+    //}
 
 }
