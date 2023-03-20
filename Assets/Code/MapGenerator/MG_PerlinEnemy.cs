cd89cdd8bd16559fc03b401be02f741990d64782
momoterry_Home <43855737+momoterry@users.noreply.github.com>
2023-03-20 21:18:56 +0800

    先加上敵人

--- Assets/Code/MapGenerator/MG_PerlinEnemy.cs
+++ Assets/Code/MapGenerator/MG_PerlinEnemy.cs
@@ -12,21 +12,21 @@ public class MG_PerlinEnemy : MG_PerlinNoise
     {
         base.FillTiles();
 
-        //for (int x = theCellMap.GetXMin(); x <= theCellMap.GetXMax(); x++)
-        //{
-        //    for (int y = theCellMap.GetYMin(); y <= theCellMap.GetYMax(); y++)
-        //    {
-        //        if (theCellMap.GetValue(x, y) == (int)MY_VALUE.LOW)
-        //        {
-        //            if (Random.value < 0.1f) 
-        //            {
-        //                Vector2Int c = theCellMap.GetCellCenterCoord(x, y);
-        //                Vector3 pos = new Vector3(c.x, 0, c.y);
-        //                BattleSystem.SpawnGameObj(stoneSpawnerRef, pos);
-        //            }
-        //        }
-        //    }
-        //}
+        for (int x = theCellMap.GetXMin(); x <= theCellMap.GetXMax(); x++)
+        {
+            for (int y = theCellMap.GetYMin(); y <= theCellMap.GetYMax(); y++)
+            {
+                if (theCellMap.GetValue(x, y) == (int)MY_VALUE.LOW)
+                {
+                    if (Random.value < 0.1f)
+                    {
+                        Vector2Int c = theCellMap.GetCellCenterCoord(x, y);
+                        Vector3 pos = new Vector3(c.x, 0, c.y);
+                        BattleSystem.SpawnGameObj(stoneSpawnerRef, pos);
+                    }
+                }
+            }
+        }
 
 
         //if (theMiniMap)
