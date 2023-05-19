8f931a320230516170f6d55fc0f046a173372f02
momoterry_LilithNB <43855737+momoterry@users.noreply.github.com>
2023-05-19 20:09:23 +0800

    調整 DollLaser 的行為，改成永遠 Follow 的型態

--- Assets/Code/Doll/DollLaser.cs
+++ Assets/Code/Doll/DollLaser.cs
@@ -6,61 +6,119 @@ public class DollLaser : DollAuto
 {
     public GameObject theLaserRef;
     protected BulletLaser myLaser;
-    //// Start is called before the first frame update
-    //void Start()
-    //{
 
-    //}
+    bool isLaser = false;
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
-
-    protected override void UpdateBattle()
+    protected override void Start()
     {
-        base.UpdateBattle();
-
-        if (myLaser && myLaser.gameObject.activeInHierarchy)
-        {
-            if (myTarget != null && myTarget.activeInHierarchy)
-            {
-                //print("myTarget alive!! " + myTarget);
-                myLaser.UpdateLaser(myTarget, transform.position);
-            }
-            else
-            {
-                myLaser.gameObject.SetActive(false);
-                //print("Target Off !!");
-            }
-        }
-    }
-
-
-    protected override void DoOneAttack()
-    {
+        base.Start();
         if (myLaser == null)
         {
             GameObject o = BattleSystem.SpawnGameObj(theLaserRef, transform.position);
             myLaser = o.GetComponent<BulletLaser>();
+            StopLaser();
         }
-        //print("Target On !!");
+    }
+
+    protected override void UpdateFollow()
+    {
+        if (myAgent)
+            myAgent.SetDestination(mySlot.position);
+
+        myFace = BattleSystem.GetPC().GetFaceDir();
+
+        if (!isLaser)
+        {
+            if (autoStateTime > 0.1f)
+            {
+                autoStateTime = 0;
+
+                if (SearchTarget())
+                {
+                    StartLaser();
+                }
+            }
+        }
+        else
+        {
+            if (myTarget == null || !myTarget.activeInHierarchy)
+            {
+                StopLaser();
+            }
+            else
+            {
+                if (Vector3.Distance(myTarget.transform.position, transform.position) > AttackRangeOut)
+                {
+                    StopLaser();
+                }
+                else
+                {
+                    myLaser.UpdateLaser(myTarget, transform.position);
+                }
+            }
+        }
+    }
+
+    protected void StartLaser()
+    {
+        isLaser = true;
         myLaser.gameObject.SetActive(true);
         Vector3 td = myTarget.transform.position - transform.position;
         myLaser.InitValue(DAMAGE_GROUP.PLAYER, myDamage, td, myTarget);
         myLaser.UpdateLaser(myTarget, transform.position);
     }
 
+    protected void StopLaser()
+    {
+        isLaser = false;
+        myLaser.gameObject.SetActive(false);
+    }
+
+    //protected override void UpdateBattle()
+    //{
+    //    base.UpdateBattle();
+
+    //    if (myLaser && myLaser.gameObject.activeInHierarchy)
+    //    {
+    //        if (myTarget != null && myTarget.activeInHierarchy)
+    //        {
+    //            //print("myTarget alive!! " + myTarget);
+    //            myLaser.UpdateLaser(myTarget, transform.position);
+    //        }
+    //        else
+    //        {
+    //            myLaser.gameObject.SetActive(false);
+    //            //print("Target Off !!");
+    //        }
+    //    }
+    //}
+
+
+    //protected override void DoOneAttack()
+    //{
+    //    if (myLaser == null)
+    //    {
+    //        GameObject o = BattleSystem.SpawnGameObj(theLaserRef, transform.position);
+    //        myLaser = o.GetComponent<BulletLaser>();
+    //    }
+    //    //print("Target On !!");
+    //    myLaser.gameObject.SetActive(true);
+    //    Vector3 td = myTarget.transform.position - transform.position;
+    //    myLaser.InitValue(DAMAGE_GROUP.PLAYER, myDamage, td, myTarget);
+    //    myLaser.UpdateLaser(myTarget, transform.position);
+    //}
+
     protected override void OnDeath()
     {
         //print("被打死了 !!");
-        if (myLaser)
-        {
-            //print("雷射清除");
-            Destroy(myLaser.gameObject);
-            myLaser = null;
-        }
+        //if (myLaser)
+        //{
+        //    //print("雷射清除");
+        //    Destroy(myLaser.gameObject);
+        //    myLaser = null;
+        //}
+
+        StopLaser();
         base.OnDeath();
     }
 }
