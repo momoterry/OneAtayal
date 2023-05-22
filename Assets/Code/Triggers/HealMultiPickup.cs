5c7a9fd3f821d881a6fcd6be3b7f185147ae0029
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2023-05-22 13:44:23 +0800

    Blood Orb 素材，並加入測試關卡架構

---
+++ Assets/Code/Triggers/HealMultiPickup.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealMultiPickup : MonoBehaviour
+{
+    public int healTotal = 100;
+
+    void OnTG(GameObject whoTG)
+    {
+        print("heal....");
+    }
+}
