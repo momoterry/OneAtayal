c317747d9e644e9bdac4b737eaec3e0a43215251
momoterry_Home <43855737+momoterry@users.noreply.github.com>
2023-06-07 15:04:37 +0800

    加入敵人血量

---
+++ Assets/Code/AI/EnemyBody.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBody : Enemy
+{
+    protected override void UpdateIdle()
+    {
+        //什麼都不用做
+    }
+}
