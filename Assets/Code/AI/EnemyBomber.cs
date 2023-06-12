87a70bd2a9d51b41c6a10330632b07aa73a82583
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2023-06-12 16:19:29 +0800

    炸彈兵爆炸行為

---
+++ Assets/Code/AI/EnemyBomber.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBomber : Enemy
+{
+    public float expRadius = 4.0f;
+    public GameObject expFX;
+    public GameObject hitFX;
+
+    protected float bombTime = 1.0f;
+
+    protected override void UpdateAttack()
+    {
+        bombTime -= Time.deltaTime;
+        if (bombTime < 0)
+        {
+            DoExplosion();
+            Destroy(gameObject);
+        }
+    }
+
+    protected void DoExplosion()
+    {
+        Vector3 expPos = transform.position;
+
+        BattleSystem.SpawnGameObj(expFX, expPos);
+
+        //Ãz¬µ³B²z
+        Collider[] cols = Physics.OverlapSphere(expPos, expRadius);
+        foreach (Collider co in cols)
+        {
+            if (co.gameObject.CompareTag("Player") || co.gameObject.CompareTag("Doll"))
+            {
+                co.gameObject.SendMessage("OnDamage", myDamage);
+                BattleSystem.SpawnGameObj(hitFX, co.transform.position);
+            }
+        }
+    }
+}
