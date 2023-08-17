d957e83671fae871e9380e912af12331c2792d00
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2023-08-17 12:17:37 +0800

    DollStone 的行為改成近距離 Dash

--- Assets/Code/Doll/Doll_Dasher.cs
+++ Assets/Code/Doll/Doll_Dasher.cs
@@ -1,12 +1,147 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Doll_Dasher : DollAuto
 {
+    //public float DashSpeed = 10.0f;
+    public float DashTime = 0.1f;
+    public float BackTime = 0.1f;
+    public float MinDashLength = 1.0f;
+    public float DashToTargetDistance = 0.5f;
+
+    public GameObject hitFX;
+
+    //protected float originalSpeed = 3.0f;
+    //protected float originalAcc = 10.0f;
+
+    public Transform myDashBody;
+
+    protected float dashStateTime = 0;
+    protected Vector3 dashVector;
+    protected Vector3 bodyOriginalRelativePos;
+
+    public enum DASH_STATE
+    {
+        NONE,
+        DASHING,
+        BACK,
+    }
+
+    DASH_STATE currDashState = DASH_STATE.NONE;
+    DASH_STATE nextDashState = DASH_STATE.NONE;
+
     protected override void DoOneAttack()
     {
-        base.DoOneAttack();
+        if (myDashBody)
+            bodyOriginalRelativePos = myDashBody.transform.position - transform.position;
 
+        dashVector = myTarget.transform.position - transform.position;
+        dashVector.y = 0;
+
+        float dashLength = Mathf.Max(MinDashLength, dashVector.magnitude - DashToTargetDistance);
+        dashVector = dashVector.normalized * dashLength;
+        //print("DashLength : " + dashLength);
+
+        nextDashState = DASH_STATE.DASHING;
+    }
+
+    protected void DoFinishDash()
+    {
+        if (myDashBody)
+        {
+            myDashBody.position = transform.position + bodyOriginalRelativePos;
+        }
+    }
+
+    protected void DoApplyDashDamage()
+    {
+        if (myTarget)
+        {
+            myTarget.SendMessage("OnDamage", myDamage);
+
+            if (hitFX)
+            {
+                Vector3 pos = myTarget.transform.position;
+                BattleSystem.GetInstance().SpawnGameplayObject(hitFX, pos, false);
+            }
+        }
+    }
+
+    protected override void UpdateAttack()
+    {
+        //base.UpdateAttack();
+
+        if (nextDashState != currDashState)
+        {
+            switch (nextDashState)
+            {
+                case DASH_STATE.DASHING:
+                    DoApplyDashDamage();
+                    break;
+                case DASH_STATE.BACK:
+                    break;
+                case DASH_STATE.NONE:
+                    DoFinishDash();
+                    break;
+            }
+            dashStateTime = 0;
+
+            currDashState = nextDashState;
+        }
+        else
+        {
+            dashStateTime += Time.deltaTime;
+        }
+
+
+        switch (currDashState)
+        {
+            case DASH_STATE.NONE:
+                base.UpdateAttack();
+                break;
+            case DASH_STATE.DASHING:
+                timeToAttack -= Time.deltaTime;
+                UpdateDash();
+                break;
+            case DASH_STATE.BACK:
+                timeToAttack -= Time.deltaTime;
+                UpdateBack();
+                break;
+        }
+
+    }
+
+    protected void UpdateDash()
+    {
+        float posRatio = dashStateTime / DashTime;
+        if (posRatio >= 1.0f)
+        {
+            nextDashState = DASH_STATE.BACK;
+            posRatio = 1.0f;
+        }
+
+        Vector3 localPos = dashVector * posRatio;
+        if (myDashBody)
+        {
+            myDashBody.position = transform.position + bodyOriginalRelativePos + localPos;
+        }
+    }
+
+    protected void UpdateBack()
+    {
+        float posRatio = 1.0f - (dashStateTime / BackTime);
+        if (posRatio <= .0)
+        {
+            nextDashState = DASH_STATE.NONE;
+            posRatio = 0;
+        }
+
+        Vector3 localPos = dashVector * posRatio;
+        if (myDashBody)
+        {
+            myDashBody.position = transform.position + bodyOriginalRelativePos + localPos;
+        }
     }
 }
