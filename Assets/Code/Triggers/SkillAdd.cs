8a64092ae7a9de37a418b91715d9a263ca4b944c
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2023-09-01 14:51:26 +0800

    初步的更改戰鬥升級技能架構

---
+++ Assets/Code/Triggers/SkillAdd.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillAdd : MonoBehaviour
+{
+    public SkillBase skillOneToAdd;
+
+    void OnTG(GameObject whoTG)
+    {
+        const int maxSkill = 4;
+
+        PC_One thePC = BattleSystem.GetInstance().GetPlayer().GetComponent<PC_One>();
+        if (thePC == null)
+        {
+            print("ERROR!! SkillAdd only supoort PC_One!!!!");
+            return;
+        }
+
+        if (skillOneToAdd)
+        {
+            thePC.SetActiveSkill(skillOneToAdd, 1);
+            //for (int i = 1; i < maxSkill; i++)
+            //{
+            //    if (thePC.activeSkillRefs[i] == null)
+            //    {
+            //        print("找到空的欄位: " + i);
+            //        thePC.SetActiveSkill(skillOneToAdd, i);
+            //    }
+            //}
+        }
+        else
+        {
+            for (int i = 1; i < maxSkill; i++)
+            {
+                thePC.SetActiveSkill(null, i);
+            }
+        }
+
+        whoTG.SendMessage("OnActionResult", true, SendMessageOptions.DontRequireReceiver);      //TODO: 改用 Trigger 的方式回應
+
+    }
+}
