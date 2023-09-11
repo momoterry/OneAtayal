f1ed99cc8f0d04a8041c3f337d0117dd48d9e469
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2023-09-11 16:47:42 +0800

    更正名稱 Blazer => Blaze

--- Assets/Code/bullet/BulletBlaze.cs
+++ Assets/Code/bullet/BulletBlaze.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletBlazer : bullet_base
+public class BulletBlaze : bullet_base
 {
     public GameObject Step_Object;
 
