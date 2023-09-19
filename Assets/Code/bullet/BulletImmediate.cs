7929d82c398d2c00dbe7cec490b2d00ade1df23c
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2023-09-19 13:05:55 +0800

    DollThunder 的校調

--- Assets/Code/bullet/BulletImmediate.cs
+++ Assets/Code/bullet/BulletImmediate.cs
@@ -1,25 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class BulletImmediate : bullet_base
 {
     public GameObject fxRef;
+    public GameObject castFxRef;
+    public bool fxLinkTarget = true;
+    public float randomDelayMax = 0;
+
+    protected float waitTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (targetObj)
+        if (randomDelayMax > 0)
         {
-            if (fxRef)
-                BattleSystem.SpawnGameObj(fxRef, targetObj.transform.position);
-            targetObj.SendMessage("OnDamage", myDamage);
+            waitTime = Random.Range(0, randomDelayMax);
+            print("Wait Time: " + waitTime);
         }
-        Destroy(gameObject);
+        else
+            DoDamage();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+            if (waitTime <= 0)
+                DoDamage();
+        }
     }
+
+    protected void DoDamage()
+    {
+        if (targetObj)
+        {
+            if (castFxRef && myDamage.Owner)
+            {
+                GameObject fxC = BattleSystem.SpawnGameObj(castFxRef, myDamage.Owner.transform.position);
+                if (fxC)
+                    fxC.transform.parent = myDamage.Owner.transform;
+            }
+
+            if (fxRef)
+            {
+                GameObject fxO = BattleSystem.SpawnGameObj(fxRef, targetObj.transform.position);
+                if (fxLinkTarget && fxO)
+                    fxO.transform.parent = targetObj.transform;
+            }
+            targetObj.SendMessage("OnDamage", myDamage);
+        }
+        Destroy(gameObject);
+    }
+
 }
