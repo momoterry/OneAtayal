0c79c0499f467a9a89cf71140d30539ee814f6b1
momoterry_Home <43855737+momoterry@users.noreply.github.com>
2023-10-20 18:03:56 +0800

    敵 Buff 架構先行

---
+++ Assets/Code/Buff/BuffAura.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 目前先實作成 Enemy 專用的 Buff 靈氣
+// 等和 DollBuff 整合後，可以成為通用的 BuffAura
+
+public class BuffAura : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
