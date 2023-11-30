86044ebbc0df0a821b02247db8e1f867e16c23c5
momoterry_Home <43855737+momoterry@users.noreply.github.com>
2023-11-30 14:58:43 +0800

    關卡重置的功能 (Trigger)

---
+++ Assets/Code/Triggers/MapReset.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapReset : MonoBehaviour
+{
+    public string mapNameToRest;
+
+    void OnTG(GameObject whoTG)
+    {
+        GameSystem.GetPlayerData().SaveMap(mapNameToRest, null);
+
+        SystemUI.ShowMessageBox(null, "Ãö¥d­«¸m: " + mapNameToRest);
+    }
+}
