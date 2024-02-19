73c2f5f241e5f20b92473fd3ad28b0537532cabc
momoterry_LilithNB <43855737+momoterry@users.noreply.github.com>
2024-02-19 23:48:18 +0800

    MazeOneCave 自動根據 RandomWalker 的結果校正 PuzzleMap 大小

--- Assets/Code/MapGenerator/MG_MazeOneCave.cs
+++ Assets/Code/MapGenerator/MG_MazeOneCave.cs
@@ -19,17 +19,34 @@ public class MG_MazeOneCave : MG_MazeOne
     }
     public MAZE_DIR mazeDir = MAZE_DIR.INSIDE_OUT;
 
-    //RandamWalkerMap theRWMap = null;
+    protected RandamWalkerMap theRWMap = null;
+    protected Vector2Int rwMapShift = Vector2Int.zero;
 
     protected override void PresetMapInfo()
     {
         //嘗試先計算 RWalkerMap
+        theRWMap = new RandamWalkerMap();
+        theRWMap.blockFillRatio = blockFillRatio;
+        theRWMap.CreateRandomWalkerMap(puzzleWidth, puzzleHeight);
+
+        //根據計算結果重新校正地圖大小
+        rwMapShift = new Vector2Int(-theRWMap.xMin, -theRWMap.yMin);
+        puzzleWidth = theRWMap.xMax - theRWMap.xMin + 1;
+        puzzleHeight = theRWMap.yMax - theRWMap.yMin + 1;
+
+
         if (extendTerminal)
         {
             if (mazeDir == MAZE_DIR.DONW_TO_TOP || mazeDir == MAZE_DIR.TOP_TO_DOWN)
+            {
                 puzzleHeight += 2;
+                rwMapShift.y++;
+            }
             else if (mazeDir == MAZE_DIR.LEFT_TO_RIGHT || mazeDir == MAZE_DIR.RIGHT_TO_LEFT)
+            {
                 puzzleWidth += 2;
+                rwMapShift.x++;
+            }
         }
         base.PresetMapInfo();
     }
@@ -38,87 +55,95 @@ public class MG_MazeOneCave : MG_MazeOne
     {
         base.InitPuzzleMap();
 
-        //puzzleStart.x = puzzleWidth / 2;
-        //puzzleStart.y = puzzleHeight / 2;
+        //RandamWalkerMap theRWMap = new RandamWalkerMap();
+        //theRWMap.blockFillRatio = blockFillRatio;
+        //int rwWidth = puzzleWidth;
+        //int rwHeight = puzzleHeight;
+        ////Vector2Int mapShift = Vector2Int.zero;
+        //if (extendTerminal)
+        //{
+        //    if (mazeDir == MAZE_DIR.DONW_TO_TOP || mazeDir == MAZE_DIR.TOP_TO_DOWN)
+        //    {
+        //        //rwHeight -= 2;
+        //        //mapShift = new Vector2Int(0, 1);
+        //        for (int i = 0; i < puzzleWidth; i++)
+        //        {
+        //            puzzleMap[i][0].value = cellInfo.INVALID;
+        //            puzzleMap[i][puzzleHeight - 1].value = cellInfo.INVALID;
+        //        }
+        //    }
+        //    else if (mazeDir == MAZE_DIR.LEFT_TO_RIGHT || mazeDir == MAZE_DIR.RIGHT_TO_LEFT)
+        //    {
+        //        //rwWidth -= 2;
+        //        //mapShift = new Vector2Int(1, 0);
+        //        for (int i = 0; i < puzzleHeight; i++)
+        //        {
+        //            puzzleMap[0][i].value = cellInfo.INVALID;
+        //            puzzleMap[puzzleWidth - 1][i].value = cellInfo.INVALID;
+        //        }
+        //    }
+        //}
 
-        RandamWalkerMap theRWMap = new RandamWalkerMap();
-        theRWMap.blockFillRatio = blockFillRatio;
-        int rwWidth = puzzleWidth;
-        int rwHeight = puzzleHeight;
-        Vector2Int mapShift = Vector2Int.zero;
-        if (extendTerminal)
+        //theRWMap.CreateRandomWalkerMap(rwWidth, rwHeight);//, puzzleStart - mapShift);
+        puzzleStart = theRWMap.vStart + rwMapShift;
+        puzzleEnd = theRWMap.vEnd + rwMapShift;
+
+        //List<Vector2Int> leftMost = new List<Vector2Int>();
+        //List<Vector2Int> rightMost = new List<Vector2Int>();
+        //List<Vector2Int> topMost = new List<Vector2Int>();
+        //List<Vector2Int> downMost = new List<Vector2Int>();
+        //int xMin = int.MaxValue, yMin = int.MaxValue;
+        //int xMax = int.MinValue, yMax = int.MinValue;
+
+        for (int i=0; i<puzzleWidth; i++)
         {
-            if (mazeDir == MAZE_DIR.DONW_TO_TOP || mazeDir == MAZE_DIR.TOP_TO_DOWN)
+            for (int j=0; j<puzzleHeight; j++)
             {
-                rwHeight -= 2;
-                mapShift = new Vector2Int(0, 1);
-                for (int i = 0; i < puzzleWidth; i++)
-                {
-                    puzzleMap[i][0].value = cellInfo.INVALID;
-                    puzzleMap[i][puzzleHeight - 1].value = cellInfo.INVALID;
-                }
-            }
-            else if (mazeDir == MAZE_DIR.LEFT_TO_RIGHT || mazeDir == MAZE_DIR.RIGHT_TO_LEFT)
-            {
-                rwWidth -= 2;
-                mapShift = new Vector2Int(1, 0);
-                for (int i = 0; i < puzzleHeight; i++)
-                {
-                    puzzleMap[0][i].value = cellInfo.INVALID;
-                    puzzleMap[puzzleWidth - 1][i].value = cellInfo.INVALID;
-                }
+                puzzleMap[i][j].value = cellInfo.INVALID;
             }
         }
 
-        theRWMap.CreateRandomWalkerMap(rwWidth, rwHeight);//, puzzleStart - mapShift);
-        puzzleStart = theRWMap.vStart + mapShift;
-        puzzleEnd = theRWMap.vEnd + mapShift;
-
-        List<Vector2Int> leftMost = new List<Vector2Int>();
-        List<Vector2Int> rightMost = new List<Vector2Int>();
-        List<Vector2Int> topMost = new List<Vector2Int>();
-        List<Vector2Int> downMost = new List<Vector2Int>();
-        int xMin = int.MaxValue, yMin = int.MaxValue;
-        int xMax = int.MinValue, yMax = int.MinValue;
+        print("rwMapShift: " + rwMapShift);
         for (int i = 0; i < theRWMap.rwWidth; i++)
         {
             for (int j = 0; j < theRWMap.rwHeight; j++)
             {
-                if (theRWMap.rwMap[i, j] == 0)
+                if (theRWMap.rwMap[i, j] != 0)
                 {
-                    puzzleMap[i + mapShift.x][j + mapShift.y].value = cellInfo.INVALID;
+                    print("設定 " + (i+rwMapShift.x) + ", " + (j + rwMapShift.y));
+                    puzzleMap[i + rwMapShift.x][j + rwMapShift.y].value = cellInfo.NORMAL;
                 }
-                else
-                {
-                    if (i <= xMin)  //為最左邊端點 
-                    {
-                        if (i < xMin)
-                            leftMost.Clear();
-                        leftMost.Add(new Vector2Int(i, j));
-                        xMin = i;
-                    }
-                    if (i >= xMax)  //為最右邊端點 
-                    {
-                        if (i > xMax)
-                            rightMost.Clear();
-                        rightMost.Add(new Vector2Int(i, j));
-                        xMax = i;
-                    }
-                    if (j <= yMin)  //為最下面端點
-                    {
-                        if (j < yMin)
-                            downMost.Clear();
-                        downMost.Add(new Vector2Int(i, j));
-                        yMin = j;
-                    }
-                    if (j >= yMax)  //為最上面端點
-                    {
-                        if (j > yMax)
-                            topMost.Clear();
-                        topMost.Add(new Vector2Int(i, j));
-                        yMax = j;
-                    }
-                }
+            //    else
+            //    {
+            //        if (i <= xMin)  //為最左邊端點 
+            //        {
+            //            if (i < xMin)
+            //                leftMost.Clear();
+            //            leftMost.Add(new Vector2Int(i, j));
+            //            xMin = i;
+            //        }
+            //        if (i >= xMax)  //為最右邊端點 
+            //        {
+            //            if (i > xMax)
+            //                rightMost.Clear();
+            //            rightMost.Add(new Vector2Int(i, j));
+            //            xMax = i;
+            //        }
+            //        if (j <= yMin)  //為最下面端點
+            //        {
+            //            if (j < yMin)
+            //                downMost.Clear();
+            //            downMost.Add(new Vector2Int(i, j));
+            //            yMin = j;
+            //        }
+            //        if (j >= yMax)  //為最上面端點
+            //        {
+            //            if (j > yMax)
+            //                topMost.Clear();
+            //            topMost.Add(new Vector2Int(i, j));
+            //            yMax = j;
+            //        }
+            //    }
             }
         }
 
@@ -130,23 +155,23 @@ public class MG_MazeOneCave : MG_MazeOne
         switch (mazeDir)
         {
             case MAZE_DIR.DONW_TO_TOP:
-                puzzleStart = downMost[Random.Range(0, downMost.Count)] + mapShift;
-                puzzleEnd = topMost[Random.Range(0, topMost.Count)] + mapShift;
+                puzzleStart = theRWMap.downMost[Random.Range(0, theRWMap.downMost.Count)] + rwMapShift;
+                puzzleEnd = theRWMap.topMost[Random.Range(0, theRWMap.topMost.Count)] + rwMapShift;
                 BattleSystem.GetInstance().initPlayerDirAngle = 0;
                 break;
             case MAZE_DIR.TOP_TO_DOWN:
-                puzzleStart = topMost[Random.Range(0, topMost.Count)] + mapShift;
-                puzzleEnd = downMost[Random.Range(0, downMost.Count)] + mapShift;
+                puzzleStart = theRWMap.topMost[Random.Range(0, theRWMap.topMost.Count)] + rwMapShift;
+                puzzleEnd = theRWMap.downMost[Random.Range(0, theRWMap.downMost.Count)] + rwMapShift;
                 BattleSystem.GetInstance().initPlayerDirAngle = 180;
                 break;
             case MAZE_DIR.LEFT_TO_RIGHT:
-                puzzleStart = leftMost[Random.Range(0, leftMost.Count)] + mapShift;
-                puzzleEnd = rightMost[Random.Range(0, rightMost.Count)] + mapShift;
+                puzzleStart = theRWMap.leftMost[Random.Range(0, theRWMap.leftMost.Count)] + rwMapShift;
+                puzzleEnd = theRWMap.rightMost[Random.Range(0, theRWMap.rightMost.Count)] + rwMapShift;
                 BattleSystem.GetInstance().initPlayerDirAngle = 90;
                 break;
             case MAZE_DIR.RIGHT_TO_LEFT:
-                puzzleStart = rightMost[Random.Range(0, rightMost.Count)] + mapShift;
-                puzzleEnd = leftMost[Random.Range(0, leftMost.Count)] + mapShift;
+                puzzleStart = theRWMap.rightMost[Random.Range(0, theRWMap.rightMost.Count)] + rwMapShift;
+                puzzleEnd = theRWMap.leftMost[Random.Range(0, theRWMap.leftMost.Count)] + rwMapShift;
                 BattleSystem.GetInstance().initPlayerDirAngle = -90;
                 break;
         }
@@ -343,6 +368,13 @@ public class RandamWalkerMap
     public Vector2Int vStart;
     public Vector2Int vEnd;
     protected int endDist;
+    //========================== 邊界值計算用 ====================================
+    public List<Vector2Int> leftMost = new List<Vector2Int>();
+    public List<Vector2Int> rightMost = new List<Vector2Int>();
+    public List<Vector2Int> topMost = new List<Vector2Int>();
+    public List<Vector2Int> downMost = new List<Vector2Int>();
+    public int xMin = int.MaxValue, yMin = int.MaxValue;
+    public int xMax = int.MinValue, yMax = int.MinValue;
 
     public void CreateRandomWalkerMap(int mapWidth, int mapHeight)//, Vector2Int _vStart)//, ref Vector2Int _vEnd)
     {
@@ -372,6 +404,39 @@ public class RandamWalkerMap
         }
         //print("RandomWalker Total Block:" + blockNum);
         //_vEnd = vEnd;
+
+    }
+
+    protected void CheckTernima(Vector2Int pos)
+    {
+        if (pos.x <= xMin)  //為最左邊端點 
+        {
+            if (pos.x < xMin)
+                leftMost.Clear();
+            leftMost.Add(pos);
+            xMin = pos.x;
+        }
+        if (pos.x >= xMax)  //為最右邊端點 
+        {
+            if (pos.x > xMax)
+                rightMost.Clear();
+            rightMost.Add(pos);
+            xMax = pos.x;
+        }
+        if (pos.y <= yMin)  //為最下面端點
+        {
+            if (pos.y < yMin)
+                downMost.Clear();
+            downMost.Add(pos);
+            yMin = pos.y;
+        }
+        if (pos.y >= yMax)  //為最上面端點
+        {
+            if (pos.y > yMax)
+                topMost.Clear();
+            topMost.Add(pos);
+            yMax = pos.y;
+        }
     }
 
     protected bool UpdateWalkers()
@@ -386,7 +451,6 @@ public class RandamWalkerMap
                 blockNum++;
 
                 //先找最遠的當終點
-
                 int currDist = Mathf.Abs(walker.pos.x - vStart.x) + Mathf.Abs(walker.pos.y - vStart.y);
                 if (currDist > endDist)
                 {
@@ -394,6 +458,8 @@ public class RandamWalkerMap
                     endDist = currDist;
                     //print("curr endDis: " + endDist + "start " + puzzleStart + " end " + puzzleEnd);
                 }
+
+                CheckTernima(walker.pos);
 
                 isCellGen = true;
                 if (blockNum == blockNumMax)
