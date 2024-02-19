98701f8998b41d4be595559bdbe7bf81ce72c4e9
momoterry_LilithNB <43855737+momoterry@users.noreply.github.com>
2024-02-19 23:52:42 +0800

    Code 清除

--- Assets/Code/MapGenerator/MG_MazeOneCave.cs
+++ Assets/Code/MapGenerator/MG_MazeOneCave.cs
@@ -55,46 +55,7 @@ public class MG_MazeOneCave : MG_MazeOne
     {
         base.InitPuzzleMap();
 
-        //RandamWalkerMap theRWMap = new RandamWalkerMap();
-        //theRWMap.blockFillRatio = blockFillRatio;
-        //int rwWidth = puzzleWidth;
-        //int rwHeight = puzzleHeight;
-        ////Vector2Int mapShift = Vector2Int.zero;
-        //if (extendTerminal)
-        //{
-        //    if (mazeDir == MAZE_DIR.DONW_TO_TOP || mazeDir == MAZE_DIR.TOP_TO_DOWN)
-        //    {
-        //        //rwHeight -= 2;
-        //        //mapShift = new Vector2Int(0, 1);
-        //        for (int i = 0; i < puzzleWidth; i++)
-        //        {
-        //            puzzleMap[i][0].value = cellInfo.INVALID;
-        //            puzzleMap[i][puzzleHeight - 1].value = cellInfo.INVALID;
-        //        }
-        //    }
-        //    else if (mazeDir == MAZE_DIR.LEFT_TO_RIGHT || mazeDir == MAZE_DIR.RIGHT_TO_LEFT)
-        //    {
-        //        //rwWidth -= 2;
-        //        //mapShift = new Vector2Int(1, 0);
-        //        for (int i = 0; i < puzzleHeight; i++)
-        //        {
-        //            puzzleMap[0][i].value = cellInfo.INVALID;
-        //            puzzleMap[puzzleWidth - 1][i].value = cellInfo.INVALID;
-        //        }
-        //    }
-        //}
-
-        //theRWMap.CreateRandomWalkerMap(rwWidth, rwHeight);//, puzzleStart - mapShift);
-        puzzleStart = theRWMap.vStart + rwMapShift;
-        puzzleEnd = theRWMap.vEnd + rwMapShift;
-
-        //List<Vector2Int> leftMost = new List<Vector2Int>();
-        //List<Vector2Int> rightMost = new List<Vector2Int>();
-        //List<Vector2Int> topMost = new List<Vector2Int>();
-        //List<Vector2Int> downMost = new List<Vector2Int>();
-        //int xMin = int.MaxValue, yMin = int.MaxValue;
-        //int xMax = int.MinValue, yMax = int.MinValue;
-
+        //先清空預設
         for (int i=0; i<puzzleWidth; i++)
         {
             for (int j=0; j<puzzleHeight; j++)
@@ -103,54 +64,18 @@ public class MG_MazeOneCave : MG_MazeOne
             }
         }
 
-        print("rwMapShift: " + rwMapShift);
+        //print("rwMapShift: " + rwMapShift);
         for (int i = 0; i < theRWMap.rwWidth; i++)
         {
             for (int j = 0; j < theRWMap.rwHeight; j++)
             {
                 if (theRWMap.rwMap[i, j] != 0)
                 {
-                    print("設定 " + (i+rwMapShift.x) + ", " + (j + rwMapShift.y));
+                    //print("設定 " + (i+rwMapShift.x) + ", " + (j + rwMapShift.y));
                     puzzleMap[i + rwMapShift.x][j + rwMapShift.y].value = cellInfo.NORMAL;
                 }
-            //    else
-            //    {
-            //        if (i <= xMin)  //為最左邊端點 
-            //        {
-            //            if (i < xMin)
-            //                leftMost.Clear();
-            //            leftMost.Add(new Vector2Int(i, j));
-            //            xMin = i;
-            //        }
-            //        if (i >= xMax)  //為最右邊端點 
-            //        {
-            //            if (i > xMax)
-            //                rightMost.Clear();
-            //            rightMost.Add(new Vector2Int(i, j));
-            //            xMax = i;
-            //        }
-            //        if (j <= yMin)  //為最下面端點
-            //        {
-            //            if (j < yMin)
-            //                downMost.Clear();
-            //            downMost.Add(new Vector2Int(i, j));
-            //            yMin = j;
-            //        }
-            //        if (j >= yMax)  //為最上面端點
-            //        {
-            //            if (j > yMax)
-            //                topMost.Clear();
-            //            topMost.Add(new Vector2Int(i, j));
-            //            yMax = j;
-            //        }
-            //    }
             }
         }
-
-        //print("最左邊端點: " + leftMost.Count);
-        //print("最右邊端點: " + rightMost.Count);
-        //print("最下邊端點: " + downMost.Count);
-        //print("最上邊端點: " + topMost.Count);
 
         switch (mazeDir)
         {
@@ -173,6 +98,10 @@ public class MG_MazeOneCave : MG_MazeOne
                 puzzleStart = theRWMap.rightMost[Random.Range(0, theRWMap.rightMost.Count)] + rwMapShift;
                 puzzleEnd = theRWMap.leftMost[Random.Range(0, theRWMap.leftMost.Count)] + rwMapShift;
                 BattleSystem.GetInstance().initPlayerDirAngle = -90;
+                break;
+            default:
+                puzzleStart = theRWMap.vStart + rwMapShift;
+                puzzleEnd = theRWMap.vEnd + rwMapShift;
                 break;
         }
 
@@ -205,147 +134,6 @@ public class MG_MazeOneCave : MG_MazeOne
 
     ////========================== RandomWalker 參數 =============================
     public float blockFillRatio = 0.35f;
-    //protected int maxWalkers = 6;
-    //protected int initWalkerNum = 1;
-    //protected float changeDirRatio = 0.2f;
-    //protected float deadRatio = 0.15f;
-    //protected float cloneRatio = 0.2f;
-    ////========================== Random Walker 演算法 =============================
-    //protected List<RandomWalker> walkerList = new List<RandomWalker>();
-    //protected int blockNum = 0;
-    //protected int blockNumMax;
-    //protected int[,] rwMap;
-    //protected int rwWidth, rwHeight;
-    //protected int rwXMin, rwXMax, rwYMin, rwYMax;
-
-    //protected int endDist;
-
-    //protected void CreateRandomWalkerMap()
-    //{
-    //    rwWidth = puzzleWidth;
-    //    rwHeight = puzzleHeight;
-    //    rwMap = new int[rwWidth, rwHeight];
-    //    rwXMin = rwYMin = 0;
-    //    rwXMax = rwWidth - 1;
-    //    rwYMax = rwHeight - 1;
-
-    //    blockNumMax = (int)(rwWidth * rwHeight * blockFillRatio);
-
-    //    for (int i = 0; i < initWalkerNum; i++)
-    //    {
-    //        RandomWalker w = new RandomWalker(puzzleStart);
-    //        walkerList.Add(w);
-    //    }
-
-    //    puzzleEnd = puzzleStart;
-    //    endDist = 0;
-    //    int step = 0;
-    //    while (blockNum < blockNumMax && step < 10000)
-    //    {
-    //        UpdateWalkers();
-    //        step++;
-    //    }
-    //    //print("RandomWalker Total Block:" + blockNum);
-
-    //}
-
-    //protected bool UpdateWalkers()
-    //{
-    //    bool isCellGen = false;
-    //    foreach (RandomWalker walker in walkerList)
-    //    {
-    //        if (rwMap[walker.pos.x, walker.pos.y] == 0)
-    //        {
-    //            rwMap[walker.pos.x, walker.pos.y] = 2;
-    //            blockNum++;
-
-    //            //先找最遠的當終點
-
-    //            int currDist = Mathf.Abs(walker.pos.x - puzzleStart.x) + Mathf.Abs(walker.pos.y - puzzleStart.y);
-    //            if (currDist > endDist)
-    //            {
-    //                puzzleEnd = walker.pos;
-    //                endDist = currDist;
-    //                //print("curr endDis: " + endDist + "start " + puzzleStart + " end " + puzzleEnd);
-    //            }
-
-    //            isCellGen = true;
-    //            if (blockNum == blockNumMax)
-    //            {
-    //                return true;
-    //            }
-    //        }
-    //        //移動一步
-    //        walker.pos += walker.dir;
-    //        walker.pos.x = Mathf.Max(Mathf.Min(walker.pos.x, rwXMax), rwXMin);
-    //        walker.pos.y = Mathf.Max(Mathf.Min(walker.pos.y, rwYMax), rwYMin);
-
-    //        if (Random.Range(0, 1) < changeDirRatio)
-    //        {
-    //            walker.SetRandomDir();
-    //        }
-
-    //        //float rd = Random.Range(0, 1.0f);
-    //        if (walkerList.Count > 1 && Random.Range(0, 1.0f) < deadRatio)
-    //        {
-    //            //print("Death !!");
-    //            walkerList.Remove(walker);
-    //            break;
-    //        }
-    //        if (walkerList.Count < maxWalkers && Random.Range(0, 1.0f) < cloneRatio)
-    //        {
-    //            //print("Clone !!");
-    //            RandomWalker newWalker = new RandomWalker(walker.pos);
-    //            walkerList.Add(newWalker);
-    //            break;
-    //        }
-    //    }
-    //    //print("Walker GO: ( " + walkerList.Count + " )");
-
-    //    return isCellGen;
-    //}
-
-    //public class RandomWalker
-    //{
-    //    public Vector2Int pos;
-    //    public Vector2Int dir;
-    //    public RandomWalker(Vector2Int _pos)
-    //    {
-    //        pos = _pos;
-    //        SetRandomDir();
-    //    }
-    //    public RandomWalker(Vector2Int _pos, Vector2Int _initDir)
-    //    {
-    //        pos = _pos;
-    //        if (_initDir == Vector2Int.zero)
-    //            SetRandomDir();
-    //        else
-    //            dir = _initDir;
-    //    }
-    //    public void SetRandomDir()
-    //    {
-    //        int dirSeed = Random.Range(0, 4);
-    //        switch (dirSeed)
-    //        {
-    //            case 0:
-    //                dir.x = 0;
-    //                dir.y = 1;
-    //                break;
-    //            case 1:
-    //                dir.x = -1;
-    //                dir.y = 0;
-    //                break;
-    //            case 2:
-    //                dir.x = 1;
-    //                dir.y = 0;
-    //                break;
-    //            case 3:
-    //                dir.x = 0;
-    //                dir.y = -1;
-    //                break;
-    //        }
-    //    }
-    //}
 
 }
 
