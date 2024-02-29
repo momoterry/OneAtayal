e4ba37d4205b0de861d4bc44a845f98976300add
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-02-29 14:44:38 +0800

    DustroctObj 能用 ID 來取得掉落物

--- Assets/Code/DestructObj.cs
+++ Assets/Code/DestructObj.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class DestructObj : MonoBehaviour
 {
     public GameObject debrisRef;
+    public int DropID = -1; 
     public void OnDeath()
     {
         if (debrisRef)
         {
             BattleSystem.SpawnGameObj(debrisRef, transform.position);
+        }
+        if (DropID > 0 && DropManager.GetInstance())
+        {
+            DropManager.GetInstance().DoDropByID(DropID, transform.position);
         }
         Destroy(gameObject);
     }
