5149dadac850ec325577599eb5f3c1c9e6395a2f
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-03-11 17:45:09 +0800

    召喚書商店產生物件的架構

--- Assets/Code/UI/BookShopMenu.cs
+++ Assets/Code/UI/BookShopMenu.cs
@@ -1,6 +1,8 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 //巫靈書商店介面
 //Book 表示 Doll Summon Skill ，就算改用別的方式包裝
@@ -15,14 +17,52 @@ public class BookShopMenu : MonoBehaviour
     }
 
     public Transform MenuRoot;
+    public GameObject ItemRef;
 
-    public void OpenMenu(BookItemInfo[] items)
+    protected List<GameObject> itemList = new List<GameObject>();
+    protected BookItemInfo[] bookInfos;
+
+    public void OpenMenu(BookItemInfo[] infos)
     {
+        bookInfos = infos;
+        CreateItems();
         MenuRoot.gameObject.SetActive(true);
     }
 
     public void CloseMenu()
     {
         MenuRoot.gameObject.SetActive(false);
+        ClearItems();
     }
+
+    protected void CreateItems()
+    {
+        const int numPerRow = 4;
+        const float stepWidth = 36.0f;
+        const float stepHeight = 36.0f;
+        const float startX = -54.0f;
+        const float startY = 92.0f;
+        for (int i=0; i< bookInfos.Length; i++)
+        {
+            int row = i / numPerRow;
+            int col = i % numPerRow;
+            GameObject o = Instantiate(ItemRef, MenuRoot);
+            RectTransform rt = o.GetComponent<RectTransform>();
+            if (rt)
+            {
+                rt.anchoredPosition = new Vector2(startX + (stepWidth * col), startY - (stepHeight * row));
+            }
+            o.SetActive(true);
+        }
+    }
+
+    protected void ClearItems()
+    {
+        foreach (GameObject item in itemList)
+        {
+            Destroy(item);
+        }
+        itemList.Clear();
+    }
+
 }
