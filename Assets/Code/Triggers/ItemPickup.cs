a8a2a235b48c86d030fc6f92a4e77e6bf3eaeb73
momoterry_LilithNB <43855737+momoterry@users.noreply.github.com>
2024-03-29 20:31:55 +0800

    撿到魔法書素材的訊息

--- Assets/Code/Triggers/ItemPickup.cs
+++ Assets/Code/Triggers/ItemPickup.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class ItemPickup : MonoBehaviour
 {
     public string ItemID;
+    public bool showMessage = false;
 
     public void OnTG(GameObject whoTG)
     {
 
         GameSystem.GetPlayerData().AddItem(ItemID, 1);
+
+        if (showMessage)
+        {
+            ItemInfo info = ItemDef.GetInstance().GetItemInfo(ItemID);
+            if (info!=null)
+                BattleSystem.GetPC().SaySomthing("¾ß¨ì  " + info.Name);
+        }
 
         whoTG.SendMessage("OnActionResult", true, SendMessageOptions.DontRequireReceiver);
         Destroy(gameObject);
