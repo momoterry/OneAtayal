a2222b5c289c20d2e473e1a1726b8d85f7859638
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-05-07 12:58:06 +0800

    補說明

--- Assets/Code/LevelGame/MazeGameManager.cs
+++ Assets/Code/LevelGame/MazeGameManager.cs
@@ -7,7 +7,7 @@ public class MazeGameManagerBase:MonoBehaviour
 {
     public float difficultRateMin = 1.0f;   //最小難度率，用來調整敵人數量
     public float difficultRateMax = 2.0f;   //最大難度率，用來調整敵人數量
-    public int enmeyLV = 1;
+    public int enmeyLV = 1;                 //敵人等級，目前只有針對 RoomEnemyGroup 中指定了 enemyID 的才有作用
 
     virtual public void AddRoom(Vector3 center, float width, float height, CELL_BASE cell, bool isMain, float mainRatio, float doorWidth, float doorHeight) { }
     virtual public void AddPath(Vector3 center, float width, float height, CELL_BASE cell, bool isMain, float mainRatio, float doorWidth, float doorHeight) { }
