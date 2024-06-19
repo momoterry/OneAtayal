5a7641c7dd0c229294c90e87ac10866190ff77d7
momoterry_Home <43855737+momoterry@users.noreply.github.com>
2024-06-19 22:49:01 +0800

    先建立了 EnemyBeta 和測試架構

---
+++ Assets/Code/AI/EnemyBeta.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// EnemyBeta : 能直接透過 SkillBase 來組合出技能的 AI 設定
+//
+//
+
+public class EnemyBeta : Enemy
+{
+    public SkillBase normalSkillRef;
+
+    protected SkillBase normalSkill;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        if (normalSkillRef)
+        {
+            GameObject o = Instantiate(normalSkillRef.gameObject, transform);
+            normalSkill = o.GetComponent<SkillBase>();
+            normalSkill.InitCasterInfo(gameObject);
+        }
+    }
+
+    protected override void DoOneAttack()
+    {
+        print("EnemyBeta----DoOneAttack");
+        //base.DoOneAttack();
+
+        if (normalSkill)
+        {
+            print("----normalSkill.DoStart()");
+            normalSkill.DoStart();
+        }
+    }
+
+    protected override void UpdateAttack()
+    {
+        //print("EnemyBeta----UpdateAttack");
+        base.UpdateAttack();
+    }
+}
