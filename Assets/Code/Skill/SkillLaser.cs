07613abcf6cde6437b5e0fc0f4e9545b2365203c
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-06-20 14:11:40 +0800

    SkillLaser 的實作

--- Assets/Code/Skill/SkillLaser.cs
+++ Assets/Code/Skill/SkillLaser.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class SkillLaser : SkillBase
 {
     public GameObject theLaserRef;
+    public float searchRange = 10.0f;
+    public float maxRange = 12.0f;
+    public float laserDuration = 2.0f;
+
+
     protected BulletLaser myLaser;
     protected GameObject myTarget;
+    protected float laserTime = 0;
 
     bool isLaser = false;
 
     // Start is called before the first frame update
-    override protected void Start()
+    protected override void Start()
     {
         base.Start();
         if (myLaser == null)
         {
             GameObject o = BattleSystem.SpawnGameObj(theLaserRef, transform.position);
+            o.transform.parent = transform;
             myLaser = o.GetComponent<BulletLaser>();
             StopLaser();
         }
@@ -24,17 +31,25 @@ public class SkillLaser : SkillBase
 
     public override bool DoStart(ref SKILL_RESULT result)
     {
+        if (isLaser)
+        {
+            result = SKILL_RESULT.COLL_DOWN;
+            return false;
+        }
+
         //°ò¥»ÀË¬d
         if (!base.DoStart(ref result))
             return false;
 
         //print("SkillShoot!!!!!! ");
-        myTarget = FindBestShootTarget(10.0f);
+        myTarget = FindBestShootTarget(searchRange);
         if (myTarget == null)
         {
             result = SKILL_RESULT.NO_TARGET;
             return false;
         }
+
+        StartLaser();
 
         result = SKILL_RESULT.SUCCESS;
         base.OnSkillSucess();
@@ -46,7 +61,23 @@ public class SkillLaser : SkillBase
         base.Update();
         if (isLaser)
         {
-            myLaser.UpdateLaser(myTarget, transform.position);
+            if (myTarget == null || !myTarget.activeInHierarchy)
+            {
+                StopLaser();
+            }
+            else
+            {
+                laserTime += Time.deltaTime;
+                //myLaser.UpdateLaser(myTarget, transform.position);
+                if (Vector3.Distance(myTarget.transform.position, transform.position) > maxRange || laserTime > laserDuration)
+                {
+                    StopLaser();
+                }
+                else
+                {
+                    myLaser.UpdateLaser(myTarget, transform.position);
+                }
+            }
         }
     }
     protected GameObject FindBestShootTarget(float searchRange)
@@ -69,7 +100,9 @@ public class SkillLaser : SkillBase
 
     protected void StartLaser()
     {
+        //print("StartLaser----");
         isLaser = true;
+        laserTime = 0;
         myLaser.gameObject.SetActive(true);
         Vector3 td = myTarget.transform.position - transform.position;
         myLaser.InitValue(FACTION_GROUP.PLAYER, myDamage, td, myTarget);
@@ -78,6 +111,7 @@ public class SkillLaser : SkillBase
 
     protected void StopLaser()
     {
+        //print("StopLaser");
         isLaser = false;
         myLaser.gameObject.SetActive(false);
     }
