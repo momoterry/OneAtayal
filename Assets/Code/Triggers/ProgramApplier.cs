7ed123b0fb85de1af297b93af40d7b5cb17de7af
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-07-04 13:15:48 +0800

    準備做編輯程式編輯的 Trigger

---
+++ Assets/Code/Triggers/ProgramApplier.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgramApplier : MonoBehaviour
+{
+    //public ItemMenu theMenu;
+
+    public void OnTG(GameObject whoTG)
+    {
+
+        whoTG.SendMessage("OnActionResult", true, SendMessageOptions.DontRequireReceiver);      //TODO: 改用 Trigger 的方式回應
+    }
+}
