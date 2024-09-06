377dc1be774b216afea5100ad21c37ed0dafff4f
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-09-06 15:00:31 +0800

    初步完成 MR_EnemyGroup

---
+++ Assets/Code/LevelGame/MR_EnemyGroup.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static RoomGameplayBase;
+
+
+public class MR_EnemyGroup : MR_Node
+{
+    public EnemyGroupInfo eInfo;
+    public int width;
+    public int height;
+
+    protected MazeGameManagerBase.RoomInfo theRoom;
+    protected float diffRatio = 1.0f;
+    protected int enemyLV = 1;
+
+    void Start()
+    {
+        CreateEnemyGroup();
+    }
+    protected void CreateEnemyGroup()
+    {
+        GameObject o = SpawnEnemyGroupObject(eInfo, transform.position, width, height, diffRatio, enemyLV);
+        //o.transform.position = room.vCenter;
+        o.name = "MR_EnemyGroup_" + name + "_" + (int)(diffRatio * 100.0f);
+    }
+
+
+    public override void OnSetupByRoom(MazeGameManagerBase.RoomInfo room)
+    {
+        theRoom = room;
+        base.OnSetupByRoom(room);
+        enemyLV = room.enemyLV;
+        diffRatio = room.diffAddRatio;
+        width = Mathf.Max(Mathf.RoundToInt(width * widthRatio), 1);
+        height = Mathf.Max(Mathf.RoundToInt(height * heightRatio), 1);
+    }
+}
