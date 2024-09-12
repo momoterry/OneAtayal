70c18d37b199f9a31715dea9ace1dc7d71211822
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-09-12 15:23:16 +0800

    新計算方式

--- Assets/Code/Doll/DM_DynamicBeta.cs
+++ Assets/Code/Doll/DM_DynamicBeta.cs
@@ -7,22 +7,28 @@ public class DM_DynamicBeta : DM_Dynamic
 {
     protected int[] frontLineNums = new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
-    protected int GetFrontLines( int _num)
+    protected void GetFrontLines( int _num, out int nLine, out int nLastCount)
     {
+        nLine = 0;
+        nLastCount = 0;
         if (_num <= 0)
-            return 0;
+            return;
 
-        int sum = 0;
+        //int sum = 0;
+        int nLeft = _num;
         for (int i=0; i<frontLineNums.Length; i++)
         {
-            sum += frontLineNums[i];
-            if (_num <= sum)
+            //sum += frontLineNums[i];
+            if (nLeft < frontLineNums[i])
             {
-                return i + 1;
+                nLine = i + 1;
+                nLastCount = nLeft;
+                return;
             }
+            nLeft -= frontLineNums[i];
         }
         One.LOG("超過 Front 數量上限 !!!!");
-        return frontLineNums.Length + 1;
+        return;
     }
 
     protected override void BuildFrontSlots()
@@ -34,9 +40,10 @@ public class DM_DynamicBeta : DM_Dynamic
             return;
 
         //int nLine = ((frontNum - 1) / FrontWidth) + 1;
-        int nLine = GetFrontLines(frontNum);
-        print("前排需要行數: " + nLine);
         //int lastLineCount = (frontNum - 1) % FrontWidth + 1;
+        int nLine, nLastCount;
+        GetFrontLines(frontNum, out nLine, out nLastCount);
+        print("前排需要行數: " + nLine);
         int leftCount = frontNum;
         int currIndex = 0;
 
