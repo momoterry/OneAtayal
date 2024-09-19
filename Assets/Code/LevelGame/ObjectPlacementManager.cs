acdbb595e3d33a3a99d6775b27aca27b3803a043
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-09-19 14:58:58 +0800

    OPM 能設定隨機物件

--- Assets/Code/LevelGame/ObjectPlacementManager.cs
+++ Assets/Code/LevelGame/ObjectPlacementManager.cs
@@ -8,6 +8,10 @@ using UnityEngine;
 public class ObjectPlacementManager : MonoBehaviour
 {
     public RoomObjectPlacement.ObjectInfo[] finxObjects;
+
+    public RoomObjectPlacement.ObjectInfo[] randomObjects;
+    public int randomSelectNum = 3;
+
     protected List<MazeGameManagerBase.RoomInfo> roomList = new List<MazeGameManagerBase.RoomInfo>();
 
     public void AddRooms(List<MazeGameManagerBase.RoomInfo> _list)
@@ -17,15 +21,29 @@ public class ObjectPlacementManager : MonoBehaviour
 
     public void BuildAll()
     {
-        print("ObjectPlacementManager BuildAll 總共 Room 數: " + roomList.Count);
+        //print("ObjectPlacementManager BuildAll 總共 Room 數: " + roomList.Count);
 
-        //RoomObjectPlacement theROP = new RoomObjectPlacement();
-        //theROP.gameObject.transform.parent = transform;
+        int randomCount = randomSelectNum > randomObjects.Length ? randomObjects.Length : randomSelectNum;
+
+        RoomObjectPlacement.ObjectInfo[] allObject = new RoomObjectPlacement.ObjectInfo[finxObjects.Length + randomCount];
+        for (int i = 0; i < finxObjects.Length; i++)
+        {
+            allObject[i] = finxObjects[i];
+        }
+
+        if (randomCount > 0)
+        {
+            OneUtility.Shuffle(randomObjects);
+            for (int i = 0; i< randomCount; i++)
+            {
+                allObject[finxObjects.Length + i] = randomObjects[i];
+            }
+        }
 
         GameObject o = new GameObject("ROP_Obj");
         o.transform.SetParent(transform, false);
         RoomObjectPlacement rop = o.AddComponent<RoomObjectPlacement>();
-        rop.objs = finxObjects;
+        rop.objs = allObject;
 
         foreach (MazeGameManagerBase.RoomInfo ri in roomList)
         {
