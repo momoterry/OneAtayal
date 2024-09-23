a0eeaf462bfa37e3044ee600c02681c6024e0ab5
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-09-23 17:44:33 +0800

    初步的 Doll Talk 串接

---
+++ Assets/Code/Doll/DollTalk.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollTalk : MonoBehaviour
+{
+    public Talk theTalk;
+
+    protected string strToTalk = "";
+
+    private void Update()
+    {
+        if (strToTalk != "")
+        {
+            theTalk.AddSentence(strToTalk);
+            strToTalk = "";
+        }
+    }
+
+    public void OnJoinPlayer()
+    {
+        strToTalk = "啊哈哈，我加入了喲 !!";
+    }
+}
