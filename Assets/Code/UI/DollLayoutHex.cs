4aa0bb52b5da36b6201b4af71871ed954eae33bb
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-11-07 16:19:15 +0800

    DM HEX 的 UI 連接架構

--- Assets/Code/UI/DollLayoutHex.cs
+++ Assets/Code/UI/DollLayoutHex.cs
@@ -5,4 +5,46 @@ using UnityEngine;
 public class DollLayoutHex : DollLayoutUIBase
 {
 
+    protected List<DollLayoutItem> itemList = new();
+    protected List<DollLayoutSlot> slotList = new();
+
+    protected DM_Hex dmH;
+
+    public override void SetupDollManager(DollManager dm)
+    {
+        base.SetupDollManager(dm);
+
+        dmH = (DM_Hex)dm;
+    }
+
+    public override void OpenMenu()
+    {
+        base.OpenMenu();
+    }
+
+    public override void CloseMenu()
+    {
+        ClearAll();
+        base.CloseMenu();
+    }
+
+    protected void CreateAll()
+    {
+
+    }
+
+    protected void ClearAll()
+    {
+        foreach (DollLayoutItem di in itemList)
+        {
+            Destroy(di.gameObject);
+        }
+        foreach (DollLayoutSlot ds in slotList)
+        {
+            Destroy(ds.gameObject);
+        }
+        itemList.Clear();
+        slotList.Clear();
+    }
+
 }
