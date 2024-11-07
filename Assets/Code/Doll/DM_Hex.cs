058dbb178ef3c1b1e0c3aeede9eff61a9cab84dc
momoterry_Office <43855737+momoterry@users.noreply.github.com>
2024-11-07 13:25:49 +0800

    初步的六邊形陣型測試

--- Assets/Code/Doll/DM_Hex.cs
+++ Assets/Code/Doll/DM_Hex.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class DM_Hex : DollManager
 {
+    protected int N = 4;    //糷计
+    const int MaxSlot = 70; //沮糷计
+
+    protected class Node
+    {
+        public int slotIndex;
+        public Transform slot;
+        public Doll doll;
+    }
+
     protected override void Start()
     {
-        slotNum = 40;   //TEST
+        slotNum = MaxSlot;   //TEST
         DollSlots = new Transform[slotNum];
         for (int i = 0; i < slotNum; i++)
         {
@@ -17,6 +27,37 @@ public class DM_Hex : DollManager
         }
         dolls = new Doll[slotNum];
 
+        BuildHex();
+
         //BattleSystem.GetHUD().RegisterDollLayoutUI(this);
     }
+
+    protected void BuildHex()
+    {
+        float StepX = 1.0f;
+        float StepY = 0.875f; //丁筳 Y 糴σ納罽祏
+
+        int currIndex = 0;
+        float y = StepY * N;
+        for (int i = -N; i<=N; i++)
+        {
+            int lNum = N * 2 + 1 - Mathf.Abs(i);    //–︽ Node 计
+            float x = StepX * lNum * -0.5f;
+            for (int j = 0; j<=lNum; j++)
+            {
+                Node node = new Node();
+                node.slotIndex = currIndex;
+                node.slot = DollSlots[currIndex];
+                node.doll = null;
+                node.slot.localPosition = new Vector3(x, 0, y);
+
+                currIndex++;
+                x += StepX;
+            }
+            y -= StepY;
+        }
+
+        print("羆 Build  Node 计: " + currIndex);
+    }
+
 }
