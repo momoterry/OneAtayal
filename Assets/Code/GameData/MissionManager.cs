47ee6d7fe17b0159727af63a5b9a8da35c469abf
momoterry <43855737+momoterry@users.noreply.github.com>
2024-11-26 22:57:55 +0800

    準備

--- Assets/Code/GameData/MissionManager.cs
+++ Assets/Code/GameData/MissionManager.cs
@@ -2,6 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class MissionData
+{
+    public string Title;
+    public string scene;
+    public GameManagerDataBase game;
+    public enum TYPE
+    {
+        NORMAL,     //單純走到底
+        BOSS,       //擊殺目標
+        EXPLORE,
+    }
+    public TYPE type;
+}
+
 public class MissionManager : GlobalSystemBase
 {
 
