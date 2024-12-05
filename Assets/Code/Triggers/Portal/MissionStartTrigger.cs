f3744fec030f9da15a4bb4ff921ca449cc3983df
momoterry <43855737+momoterry@users.noreply.github.com>
2024-12-05 17:39:38 +0800

    MissionStartTrigger 加入 FadeOut

--- Assets/Code/Triggers/Portal/MissionStartTrigger.cs
+++ Assets/Code/Triggers/Portal/MissionStartTrigger.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class MissionStartTrigger : MonoBehaviour
 {
-    void OnTG(GameObject whoTG)
+    public float fadeTime = 0.25f;
+    public void OnTG(GameObject whoTG)
+    {
+        whoTG.SendMessage("OnActionResult", true, SendMessageOptions.DontRequireReceiver);      //TODO: 改用 Trigger 的方式回應
+
+        BattleSystem.GetPC().ForceStop();
+        BattleSystem.GetInstance().StartFadeOut(fadeTime, OnFadeOutFinish);
+    }
+
+    public void OnFadeOutFinish()
     {
         MissionManager.StartCurrMission();
-        whoTG.SendMessage("OnActionResult", true, SendMessageOptions.DontRequireReceiver);      //TODO: 改用 Trigger 的方式回應
     }
 }
