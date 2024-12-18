967ab94e5079f972d48c94dd33d53478520c4820
momoterry_LilithNB <43855737+momoterry@users.noreply.github.com>
2024-12-18 10:05:15 +0800

    MissionObjectTrigger 架構

---
+++ Assets/Code/Triggers/MissionObjectTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionObjectTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("加入一個任務目標");
+    }
+
+    public void OnTG(GameObject whoTG)
+    {
+
+    }
+}
